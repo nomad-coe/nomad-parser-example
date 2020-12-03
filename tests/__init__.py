98d7705eae07f3bca0b450da4271fc24d87df7e3
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-12-03 11:16:07 +0100

    Added github actions for linting and tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -15,25 +15,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-import pytest
-import logging
-
-from nomad.datamodel import EntryArchive
-
-from exampleparser import ExampleParser
-
-
-@pytest.fixture
-def parser():
-    return ExampleParser()
-
-
-def test_example(parser):
-    archive = EntryArchive()
-    parser.run('tests/data/example.out', archive, logging)
-
-    run = archive.section_run[0]
-    assert len(run.section_system) == 2
-    assert len(run.section_single_configuration_calculation) == 2
-    assert run.section_single_configuration_calculation[0].x_example_magic_value == 42
